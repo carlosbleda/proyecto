2d4e49a3065d11d6cdd8dd807fea71b94765428a
carlosbleda <carlosbleda3@gmail.com>
2021-08-09 18:34:45 -0300

    segundo cambio

--- js/login.js
+++ js/login.js
@@ -2,62 +2,26 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e) {
-    //  guardar();
-    const div = document.createElement('div');
-    div.setAttribute('id', 'contenedor');
-    document.body.appendChild
-    mostrarlogin();
-});
-let usuario = [];
-let pass = "";
-let array = [];
-//function guardar(dato, pass) {
+    function guardar(dato, pass) {
 
-//if (dato.trim(dato) === "" || pass.trim(pass) === "") { //Chequea que el dato recibido no esté vacío. 
-//El método trim elimina los espacios en blanco al //inicio y al final del mismo.
-
-// } else {
-//localStorage.setItem("usuario", dato.trim()); //setItem almacena el dato en la posición "usuario"
-// localStorage.setItem("password", pass.trim()); // Almaceno la contraseña
-// sessionStorage.setItem("usuario", dato.trim());
-// console.log(" Usuario : " + dato + " Password : " + pass);
-
-
-//location.href = "categories.html";
-
-//getItem obtiene el dato almacenado en la posición "usuario"
-
-//} //
-//}
-//
-function mostrarlogin(array) {
-
-    const contenidohtml = "";
-    for (let i = 0; array.length; i++) {
-        let categoria = array[i];
-        contenidohtml += `'<form class=
-    'signin' > < /form>'
-    '<h2>'
-    'Ingrese para continuar' < /h2>'` +
-
-            `<
-    input type = text id = "dato"
-    placeholder = " Nombre de usuario"
-    required > < br > < br >` +
-
-            `<
-        input type = "password"
-    id = "pass"
-    placeholder = " Tu clave aquí "
-    required > < br > < br >` +
+        if (dato.trim() === "" || pass.trim() === "") { //Chequea que el dato recibido no esté vacío. 
+            //El método trim elimina los espacios en blanco al inicio y al final del mismo.
+            alert("El dato está vacío");
+        } else {
+            localStorage.setItem("usuario", dato.trim()); //setItem almacena el dato en la posición "usuario"
+            localStorage.setItem("password", pass.trim()); // Almaceno la contraseña
+            sessionStorage.setItem("usuario", dato.trim());
+            alert(" Usuario : " + dato + " Password : " + pass);
 
 
 
-            `<
-        button onclick = "guardar(dato.value, pass.value);" > Iniciar Sesion < /button>
-<br > < a href = "categories.html" > Ir a la siguiente < /a> < /
-        form >
-        '`
-        document.getElementById("contenedor").innerHTML = contenidohtml;
+
+            //getItem obtiene el dato almacenado en la posición "usuario"
+
+        }
     }
-}
+
+    function redireccionar() {
+        location.href = "index.html";
+    }
+});
