7b8ebb742c258d15db7872dcd7a17743d574089e
carlosbleda <carlosbleda3@gmail.com>
2021-08-13 14:38:54 -0300

    boton google

--- js/init.js
+++ js/init.js
@@ -40,16 +40,24 @@ var getJSONData = function(url) {
         });
 }
 
-var cerrarsesion = function signOut() {
-        var auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut().then(function() {
-            console.log('User signed out.');
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function() {
+        console.log('User signed out.');
 
-        });
-    }
-    //Función que se ejecuta una vez que se haya lanzado el evento de
-    //que el documento se encuentra cargado, es decir, se encuentran todos los
-    //elementos HTML presentes.
+    });
+}
+
+function mostrardatos(usuario, sesion) {
+    var usuario = localStorage.getItem("usuario"); //getItem Obtiene el dato de la posición "usuario"
+    var sesion = sessionStorage.getItem("usuario");
+    document.getElementById('user').innerHTML = usuario;
+    document.getElementById('sesion').innerHTML = sesion;
+    //Nótese que si cerramos el navegador y volvemos a abrir, el dato sigue almacenado
+}
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e) {
 
 });
