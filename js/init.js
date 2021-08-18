ae73dc7e5b4323cafdb10bdf587f770f001b749e
carlosbleda <carlosbleda3@gmail.com>
2021-08-18 18:36:48 -0300

    algo

--- js/init.js
+++ js/init.js
@@ -50,7 +50,14 @@ function signOut() {
     });
 }
 
-
+function onSignIn(googleUser) { //funcion de inicio de sesion del boton de google
+    var profile = googleUser.getBasicProfile();
+    sessionStorage.setItem('ID: ' + profile.getId())
+    sessionStorage.setItem('Name: ' + profile.getName());
+    sessionStorage.setItem('Image URL: ' + profile.getImageUrl());
+    sessionStorage.setItem('Email: ' + profile.getEmail());
+    location.href = "index.html";
+}
 
 
 function datoslogin(usuario, pass) {
--- js/login.js
+++ js/login.js
@@ -29,14 +29,7 @@ function validar() {
 
 }
 
-function onSignIn(googleUser) { //funcion de inicio de sesion del boton de google
-    var profile = googleUser.getBasicProfile();
-    sessionStorage.setItem('ID: ' + profile.getId())
-    sessionStorage.setItem('Name: ' + profile.getName());
-    sessionStorage.setItem('Image URL: ' + profile.getImageUrl());
-    sessionStorage.setItem('Email: ' + profile.getEmail());
-    location.href = "index.html";
-}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
