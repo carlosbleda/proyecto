5da01f35bd97a1e9f2e10753fca9886bfba4ad87
carlosbleda <carlosbleda3@gmail.com>
2021-08-18 17:47:36 -0300

    algo

--- js/init.js
+++ js/init.js
@@ -50,14 +50,7 @@ function signOut() {
     });
 }
 
-function onSignIn(googleUser) { //funcion de inicio de sesion del boton de google
-    var profile = googleUser.getBasicProfile();
-    sessionStorage.setItem('ID: ' + profile.getId())
-    sessionStorage.setItem('Name: ' + profile.getName());
-    sessionStorage.setItem('Image URL: ' + profile.getImageUrl());
-    sessionStorage.setItem('Email: ' + profile.getEmail());
-    location.href = "/proyecto/index.html" //redireccion a la pagina principal
-}
+
 
 
 function datoslogin(usuario, pass) {
--- js/login.js
+++ js/login.js
@@ -29,7 +29,15 @@ function validar() {
 
 }
 
-//función que se ejecuta una vez que se haya lanzado el evento de
+function onSignIn(googleUser) { //funcion de inicio de sesion del boton de google
+    var profile = googleUser.getBasicProfile();
+    sessionStorage.setItem('ID: ' + profile.getId())
+    sessionStorage.setItem('Name: ' + profile.getName());
+    sessionStorage.setItem('Image URL: ' + profile.getImageUrl());
+    sessionStorage.setItem('Email: ' + profile.getEmail());
+    location.href = "/proyecto/index.html" //redireccion a la pagina principal
+}
+//Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e) {
